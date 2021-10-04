c901c7c50e71172ca515d68638a7d63e94b5339d
Jeremi Herodian <Overload.emperor14@gmail.com>
2021-10-04 10:34:22 +0800

    Configure for deploy to Heroku

--- app.py
+++ app.py
@@ -86,7 +86,3 @@ def create_app():
     return app
 
 app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT",5000))
-    app.run(host='127.0.0.1',port=port,debug=True, threaded=True)
